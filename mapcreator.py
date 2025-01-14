64fcd67c4b5481d33aefa68b124d0d594dd9166c
Shevk <Sheasdasd@kdas.com>
2025-01-14 17:51:14 +1000

    добавил текстуру коробки и сделал отображение текстуры при создании карт

--- mapcreator.py
+++ mapcreator.py
@@ -1,16 +1,30 @@
 import pygame as pg
 from math import floor
+import os
+import sys
 from time import ctime
-import pickle
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pg.image.load(fullname)
+    return image
 
 class Board:
     # создание поля
-    def __init__(self, width, height, canvas, cell_size=30):
+    def __init__(self, width, height, canvas, cell_size=80):
+        self.images = [
+            load_image("земля.jpg"),
+            load_image("вода.jpg"),
+            load_image("кирпичи.png"),
+            load_image("коробка.jpg")]
+        self.scaled_images = [pg.transform.scale(img, (cell_size, cell_size)) for img in self.images]
         self.width = width
         self.height = height
         self.canvas = canvas
-        self.colors = ["black", "brown", "blue"]
         self.board = [[0] * width for _ in range(height)]
         self.cell_size = cell_size
         self.left = (screenw - x * cell_size) / 2
@@ -25,11 +39,16 @@ class Board:
         self.left = (screenw - x * self.cell_size) / 2
 
     def render(self):
-        for i in range(int(self.height)):
-            for j in range(int(self.width)):
-                eq = (j * self.cell_size + self.left, i * self.cell_size + self.top, self.cell_size, self.cell_size)
-                pg.draw.rect(self.canvas, self.colors[self.board[i][j]], eq)
-                pg.draw.rect(self.canvas, 'white', eq, 1)
+        for i in range(self.height):
+            for j in range(self.width):
+                rect = (
+                    j * self.cell_size + self.left,
+                    i * self.cell_size + self.top,
+                    self.cell_size,
+                    self.cell_size
+                )
+                self.canvas.blit(self.scaled_images[self.board[i][j]], rect)
+                pg.draw.rect(self.canvas, 'white', rect, 1)
 
     def get_cell(self, coords):
         x, y = coords
@@ -42,7 +61,7 @@ class Board:
 
     def on_click(self, cell_coords):
         x, y = cell_coords[0], cell_coords[1]
-        self.board[y][x] = (self.board[y][x] + 1) % 3
+        self.board[y][x] = (self.board[y][x] + 1) % 4
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
@@ -50,9 +69,9 @@ class Board:
             self.on_click(cell)
 
     def save(self):
-        with open(f'maps/{input("Введите название карты").replace(" ", "").replace(":", "")}.wmap', "wb") as f:
-            self.board.append([x, y])
-            pickle.dump(self.board, f)
+        with open(f'maps/{ctime().replace(" ", "").replace(":", "")}.wmap', "w+") as f:
+            for row in self.board:
+                f.write(str(row) + '\n')
             print("Карта сохранена")
 
 
