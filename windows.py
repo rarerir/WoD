01f240c99da571e917461aff2f8847ab64bff5aa
rarerir <electronich228@mail.ru>
2025-01-25 14:01:10 +1000

    ААА ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ

--- windows.py
+++ windows.py
@@ -163,11 +163,11 @@ class Circle(pg.sprite.Sprite):
 class Border(pg.sprite.Sprite):
     def __init__(self, x1, y1, x2, y2):
         super().__init__(all_sprites)
-        if x1 == x2:  # vertical wall
+        if x1 == x2:
             self.add(vertical_borders)
             self.image = pg.Surface([1, y2 - y1])
             self.rect = pg.Rect(x1, y1, 1, y2 - y1)
-        else:  # horizontal wall
+        else:
             self.add(horizontal_borders)
             self.image = pg.Surface([x2 - x1, 1])
             self.rect = pg.Rect(x1, y1, x2 - x1, 1)
