1239ecf57f9ee388d48cb7c9e0b4ec2e73425aaf
rarerir <electronich228@mail.ru>
2025-01-25 13:35:54 +1000

    перелопатил весь код

--- test.py
+++ test.py
@@ -1,114 +1,12 @@
-import pygame as pg
-import os
+from itertools import *
 
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (2, 29)
+def f(x, y, z, w):
+    return w and (y == (z <= (x and y)))
 
-pg.init()
+tabl = [(0, 0, 0, 0), (1, 0, 0, 0), (0, 0, 0, 1), (1, 0, 0, 1)]
 
-
-def gameLoop():
-    display_width = 1800
-    display_height = 1000
-    pg.display.set_caption("Kerbal Space Landing Simulator")
-    clock = pg.time.Clock()
-    display = pg.display.set_mode((display_width, display_height))
-    sprsp = pg.image.load('C:/Users/PC/PycharmProjects/untitled/sprspaceship.png').convert_alpha()
-    cosbg = pg.image.load('C:/Users/PC/PycharmProjects/untitled/cosmos bg.png').convert_alpha()
-    done = False
-
-class Spaceship:
-    def __init__(self, x, y, mass):
-        self.x = x
-        self.y = y
-        self.width = 139
-        self.height = 106
-        self.velx = 0
-        self.vely = 0
-        self.mass = mass
-        self.color = (255, 255, 255)
-        self.fuel = 500
-        self.mask = pg.mask.from_surface(self.spr)
-        self.angle = 0
-        self.changerot = 0
-
-    def check_controls(self):
-        if keys[pg.K_SPACE] and self.fuel > 0:
-            if self.angle > 0:
-                self.vely += 0.005 * (self.angle - 90)
-                self.velx += -0.005 * self.angle
-            else:
-                self.vely += -0.005 * (self.angle + 90)
-                self.velx += -0.005 * self.angle
-
-            self.fuel += -3
-        if keys[pg.K_LEFT] and self.angle < 90:
-            self.angle += 2
-        if keys[pg.K_RIGHT] and self.angle > -90:
-            self.angle += -2
-
-    def update_pos(self):
-        self.vely += 0.01
-        self.x += self.velx
-        self.y += self.vely
-        self.mask = pg.mask.from_surface(self.spr)
-
-    def update_rotation(self):
-        self.rspr = pg.transform.rotate(self.spr, self.angle)
-        self.changerot -= self.angle
-
-    def draw(self):
-        if self.fuel > 0:
-            pg.draw.rect(display, (255, 255, 255), (display_width - 100, 100 + 500 - self.fuel, 10, self.fuel), 0)
-        display.blit(self.rspr, (int(self.x), int(self.y)))
-
-        self.changerot = 0
-
-class Terrain(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.mask = pg.mask.from_threshold(display, (160, 160, 160))
-        self.hitbox = pg.Rect(self.x, self.y, display_width, 500)
-        self.ox = display_width // 2 - self.x // 2
-        self.oy = display_height // 2 - self.y // 2
-
-    def draw(self):
-        pg.draw.rect(display, (160, 160, 160), (self.x, self.y, display_width, 500), 0)
-
-spaceship = (Spaceship(500, 100, 1))
-terrain = (Terrain(0, 800))
-
-def redrawGameWindow():
-    display.blit(cosbg, (0, 0))
-    spaceship.draw()
-    terrain.draw()
-    pg.display.update()
-
-def check_for_collisions():
-    offset = (int(spaceship.x - terrain.ox), int(spaceship.y - terrain.oy))
-    print(offset)
-    print(spaceship.mask.overlap(terrain.mask, offset))
-    return spaceship.mask.overlap(terrain.mask, offset)
-    # return spaceship.hitbox.colliderect(terrain.hitbox)
-    # return pg.sprite.spritecollide(spaceship.spr, terrain.mask, False, pg.sprite.collide_mask)
-
-while not done:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            done = True
-
-    keys = pg.key.get_pressed()
-    mouse_pressed = pg.mouse.get_pressed()
-    x, y = pg.mouse.get_pos()
-
-    spaceship.check_controls()
-    spaceship.update_pos()
-    spaceship.update_rotation()
-    if check_for_collisions() is not None:
-        print('Hit! You\'ve hit the ground with the speed:', spaceship.vely)
-        exit()
-
-    redrawGameWindow()
-    clock.tick(60)
-
-gameLoop()
+for a in product([0, 1], repeat=5):
+    t = [(a[0], 0, 0, a[1]), (1, a[2], 1, 0), (a[3], a[4], 1, 0)]
+    if len(t) == len(set(t)):
+        if f(t[0], t[1], t[2]):
+            print(t)
---
+++ windows.py
@@ -0,0 +1,208 @@
+import sys
+import pygame as pg
+import random
+import os
+
+
+def loadWin(size, screenw, screenh):
+    #1530 830\
+    stop = False
+    logoSurf = pg.image.load('images/logo.png')
+    logoRect = logoSurf.get_rect(center = (screenw//2, screenh//2))
+    surf = pg.Surface(size)
+    surf.fill("black")
+    surfRect = surf.get_rect(center = (screenw//2, screenh//2))
+    for i in range(1, 510):
+        if stop:
+            break
+        if i <= 255:
+            surf.set_alpha(255 - i)
+        else:
+            surf.set_alpha((255 - i)*-1)
+        screen.blit(logoSurf, logoRect)
+        screen.blit(surf, surfRect)
+        pg.display.flip()
+        for j in pg.event.get():
+            if j.type == pg.KEYDOWN:
+                stop = True
+                break
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('sprites', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pg.image.load(fullname)
+    if colorkey is not None:
+        image.set_colorkey(colorkey)
+    return image
+
+
+def start_screen(size, screenw, screenh):
+    screen.fill((0, 0, 0))
+    settings = pg.transform.scale(load_image('настройки.png'), (200, 200))
+
+    font = pg.font.Font(None, 200)
+    text = font.render("ИГРАТЬ", True, (100, 255, 100))
+    text_x = screenw // 2 - text.get_width() // 2
+    text_y = screenh // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+
+    exit_font = pg.font.Font(None, 100)
+    exit_text = exit_font.render("ВЫЙТИ", True, (100, 255, 100))
+    exit_text_x = screenw // 2 - exit_text.get_width() // 2
+    exit_text_y = text_y + text.get_height() + 50
+    for i in range(20):
+        Circle(all_sprites)
+
+    while True:
+        dt = clock.tick(v)
+        screen.blit(settings, (screenw - 200, 0))
+
+        screen.blit(text, (text_x, text_y))
+        pg.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
+                                           text_w + 20, text_h + 20), 1)
+        screen.blit(exit_text, (exit_text_x, exit_text_y))
+        pg.draw.rect(screen, (0, 255, 0), (exit_text_x - 10, exit_text_y - 10,
+                                           exit_text.get_width() + 20, exit_text.get_height() + 20), 1)
+
+        all_sprites.update(dt)
+
+        trail_surface.fill((0, 0, 0, 0))
+        for sprite in boolets:
+            sprite.draw(trail_surface)
+
+        screen.blit(trail_surface, (0, 0))
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                x, y = pg.mouse.get_pos()
+                if ((text_x - 10 <= x) and (x <= text_x + text_w + 10)) and ((text_y - 10 <= y) and (
+                        y <= text_y + text_h + 10)):
+                    return 3
+                if ((screenw - 200 <= x) and (x <= screenw)) and ((0 <= y) and (y <= 200)):
+                    return 2
+                if (exit_text_x - 10 <= x and x <= exit_text_x + exit_text.get_width() + 10) and \
+                        (exit_text_y - 10 <= y and y <= exit_text_y + exit_text.get_height() + 10):
+                    return False
+        pg.display.update()
+        pg.display.flip()
+
+
+def settings_screen(size, screenw, screenh):
+    screen.fill((0, 0, 0))
+    fon = pg.transform.scale(load_image('фон_1.gif'), (screenw, screenh))
+    screen.blit(fon, (0, 0))
+
+    font = pg.font.Font(None, 100)
+    text = font.render("Настройки", True, (255, 255, 255))
+    text_rect = text.get_rect(center=(screenw // 2, screenh // 4))
+    screen.blit(text, text_rect)
+
+    back_button = font.render("Назад", True, (255, 255, 255))
+    back_x = screenw // 2 - back_button.get_width() // 2
+    back_y = screenh // 2 - back_button.get_height() // 2 + 100
+    screen.blit(back_button, (back_x, back_y))
+    pg.draw.rect(screen, (0, 255, 0), (back_x - 10, back_y - 10,
+                                       back_button.get_width() + 20, back_button.get_height() + 20), 1)
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                x, y = pg.mouse.get_pos()
+                if (back_x - 10 <= x and x <= back_x + back_button.get_width() + 10) and (
+                        back_y - 10 <= y and y <= back_y + back_button.get_height() + 10):
+                    return 1
+
+        pg.display.flip()
+        clock.tick(v)
+
+
+class Circle(pg.sprite.Sprite):
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.color = (255, 0, 0)
+        self.dx = random.choice([-1, 1]) * dt
+        self.dy = random.choice([-1, 1]) * dt
+        self.radius = 5
+        self.add(boolets)
+        self.x = random.randint(self.radius, screenw - self.radius)
+        self.y = random.randint(self.radius, screenh - self.radius)
+        self.rect = pg.Rect(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
+
+    def update(self, dt):
+        self.rect = self.rect.move(self.dx * dt, self.dy * dt)
+        if pg.sprite.spritecollideany(self, horizontal_borders):
+            self.dy = -self.dy
+        if pg.sprite.spritecollideany(self, vertical_borders):
+            self.dx = -self.dx
+        colided = pg.sprite.spritecollideany(self, boolets)
+        if colided != self:
+            colided.explode()
+            self.explode()
+
+    def draw(self, surface):
+        pg.draw.circle(surface, self.color, self.rect.center, self.radius)
+
+    def explode(self):
+        self.kill()
+        Circle(all_sprites)
+
+
+class Border(pg.sprite.Sprite):
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # vertical wall
+            self.add(vertical_borders)
+            self.image = pg.Surface([1, y2 - y1])
+            self.rect = pg.Rect(x1, y1, 1, y2 - y1)
+        else:  # horizontal wall
+            self.add(horizontal_borders)
+            self.image = pg.Surface([x2 - x1, 1])
+            self.rect = pg.Rect(x1, y1, x2 - x1, 1)
+
+
+if __name__ == "__main__":
+    pg.init()
+    # Группы спрайтов
+    all_sprites = pg.sprite.Group()
+    horizontal_borders = pg.sprite.Group()
+    vertical_borders = pg.sprite.Group()
+    boolets = pg.sprite.Group()
+    # Разрешение
+    info = pg.display.Info()
+    screenw = info.current_w
+    screenh = info.current_h
+    size = (screenw, screenh)
+    screen = pg.display.set_mode(size)
+    # Границы
+    Border(5, 5, screenw - 5, 5)
+    Border(5, screenh - 5, screenw - 5, screenh - 5)
+    Border(5, 5, 5, screenh - 5)
+    Border(screenw - 5, 5, screenw - 5, screenh - 5)
+    # Фпс
+    v = 144
+    clock = pg.time.Clock()
+
+    trail_surface = pg.Surface((screenw, screenh))
+    trail_surface.fill((0, 0, 0))
+    trail_surface.set_alpha(99)
+    dt = clock.tick(v) / 10
+
+    loadWin(size, screenw, screenh)
+    state = 1
+    while state:
+        if state == 1:
+            state = start_screen(size, screenw, screenh)
+        if state == 2:
+            state = settings_screen(size, screenw, screenh)
+        if state == 3:
+            import game
+    pg.quit()
+    sys.exit()
